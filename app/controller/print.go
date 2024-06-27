45529f0d2634e2176238e53e0bb3b4b0b832020f
rwh <1016681491@qq.com>
2024-06-27 15:17:31 +0800

    打印机对接

---
+++ app/controller/print.go
@@ -0,0 +1,57 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jadefox10200/goprint"
+	"golang-tool-api/app/models/response"
+	"net/http"
+)
+
+var Print = new(printer)
+
+type printer struct {
+}
+
+func (p printer) List(c *gin.Context) {
+	printers, err := goprint.GetPrinterNames()
+	if err != nil {
+		c.JSON(http.StatusOK, response.Fail(err.Error()))
+	}
+	c.JSON(http.StatusOK, response.Ok(printers))
+	return
+}
+
+func (p printer) PrintFile(c *gin.Context) {
+	type Data struct {
+		PrinterName string `json:"printerName"`
+		Num         int    `json:"num"`
+		FilePath    string `json:"filePath"`
+	}
+	var Param Data
+	err := c.ShouldBindJSON(&Param)
+	if err != nil {
+		c.JSON(http.StatusOK, response.ResultCustom(&response.BusinessError{
+			Code: response.RequestParamError,
+			Msg:  err.Error(),
+		}))
+		return
+	}
+	printerHarder, err := goprint.GoOpenPrinter(Param.PrinterName)
+	if err != nil {
+		c.JSON(http.StatusOK, response.Fail("打印机状态异常！"+err.Error()))
+		return
+	}
+	for i := 0; i <= Param.Num; i++ {
+		go func() {
+			err = goprint.GoPrint(printerHarder, Param.FilePath)
+			if err != nil {
+				c.JSON(http.StatusOK, response.Fail(struct {
+					Msg string `json:"msg"`
+				}{Msg: err.Error()}))
+				return
+			}
+		}()
+	}
+	c.JSON(http.StatusOK, response.Ok("success"))
+	return
+}
